68dd796e53e0c8c185bf9956375e4b1164fc1a0f
himanshu <himanshusaini0345@outlook.com>
2025-10-04 21:06:32 +0530

    .

--- app/src/main/java/com/example/main/controllers/HelloController.java
+++ app/src/main/java/com/example/main/controllers/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from AnswerSheet API!";
+        return "Hello from JavaWebPlayground and hey!";
     }
 }
